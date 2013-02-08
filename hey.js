271389c0cb5b1872b3ddde56d43175f98bd1f2a1
Mark Soper <masoper@gmail.com>
2013-02-08 11:37:08 -0500

    shifting to gh-pages

--- hey.js
+++ hey.js
@@ -1,6 +1,39 @@
 
+
+navigator.getMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia).bind(navigator);
+ 
 var onLoad = function() {
   
+  navigator.getMedia (
+ 
+   // constraints
+   {
+      'video': {"mandatory": {}, "optional": []},
+      'audio': true
+   },
+ 
+   // successCallback
+   function(localMediaStream) {
+      var audio = document.querySelector('audio');
+      audio.src = window.URL.createObjectURL(localMediaStream);
+      audio.onloadedmetadata = function(e) {
+
+         // handle audio
+
+      };
+   },
+ 
+   // errorCallback
+   function(err) {
+    console.log("getMedia error: " + err);
+   }
+ 
+);
+
+
 };
 
 document.addEventListener("DOMContentLoaded", onLoad);
